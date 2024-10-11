ea157b53d6feb2f7130016b0ed8adc8db995405e
Luerfel <mluerfel@gmail.com>
2024-10-11 17:21:49 -0300

    gerenciar carteiras comentarios

--- app.py
+++ app.py
@@ -755,7 +755,7 @@ def carteira_sacar():
 
             detalhes += f' | Taxa aplicada: R$ {taxa:.2f} | Valor líquido: R$ {valor_liquido:.2f}'
 
-            # Registrar a transação
+            # Registrar a transaçãoo
             cursor.execute('''
                 INSERT INTO transacoes (id_carteira, tipo, valor, detalhes)
                 VALUES (?, 'Saque', ?, ?)
